ca7ee176be561a0416b184a6078de0e1d53be8ba
Emily Davies <emilygdavies42@gmail.com>
2021-10-10 17:16:17 -0400

    Update scripts.js
    Added a "Pokedex" array, with 6 objects, and 4 keys for each obejct

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = 'bacon';
-document.write(favoriteFood);
-
-
+let pokemonList = [
+  { name: 'Bulbasaur', height: 7, color: 'green', types: ['grass', 'poison']},
+  { name: 'Charmander', height: 6, color: 'orange', types: ['fire']},
+  { name: 'Squirtle', height: 5, color: 'blue', types: ['water']},
+  { name: 'Pikachu', height: 4, color: 'yellow', types: ['electric']},
+  { name: 'Jigglypuff', height: 5, color: 'pink', types: ['fairy', 'normal']},
+  { name: 'Diglett', height: 2, color: 'brown', types: ['ground']},
+];
