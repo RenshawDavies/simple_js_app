8a9a8ac23182f1bebb41864bbb4ff0bdaf0b0258
Emily Davies <emilygdavies42@gmail.com>
2021-10-15 14:12:13 -0400

    Update scripts.js
    Wrapped array in IIFE with 'add' and 'getAll' functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,46 @@
-//array of Pokemon names, height, color, and types
-let pokemonList = [
-  { name: 'Bulbasaur', 
-    height: 0.7, 
-    color: 'green', 
-    types: ['grass', 'poison']},
-  { name: 'Charmander', 
-    height: 0.6, 
-    color: 'orange', 
-    types: ['fire']},
-  { name: 'Squirtle', 
-    height: 0.5, 
-    color: 'blue', 
-    types: ['water']},
-  { name: 'Pikachu', 
-    height: 0.4, 
-    color: 'yellow', 
-    types: ['electric']},
-  { name: 'Jigglypuff', 
-    height: 0.5, 
-    color: 'pink', 
-    types: ['fairy', 'normal']},
-  { name: 'Diglett', 
-    height: 0.2, 
-    color: 'brown', 
-    types: ['ground']},
-];
+// IIFE function to avoid accidentally accessing the global state
+let pokemonRepository = (function () {
+
+  //array of Pokemon names, height, color, and types
+  let pokemonList = [
+    { name: 'Bulbasaur', 
+      height: 0.7, 
+      color: 'green', 
+      types: ['grass', 'poison']},
+    { name: 'Charmander', 
+      height: 0.6, 
+      color: 'orange', 
+      types: ['fire']},
+    { name: 'Squirtle', 
+      height: 0.5, 
+      color: 'blue', 
+      types: ['water']},
+    { name: 'Pikachu', 
+      height: 0.4, 
+      color: 'yellow', 
+      types: ['electric']},
+    { name: 'Jigglypuff', 
+      height: 0.5, 
+      color: 'pink', 
+      types: ['fairy', 'normal']},
+    { name: 'Diglett', 
+      height: 0.2, 
+      color: 'brown', 
+      types: ['ground']},
+  ];
+
+    // adds to pokedex
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+    function getAll() {
+      return pokemonList;
+    }
+    return {
+      add: add,
+      getAll: getAll
+    };
+}) ();
 
 //loop used to list all Pokemon and their heights on webpage
 pokemonList.forEach(function(pokemon) {
